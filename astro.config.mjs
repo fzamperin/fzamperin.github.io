4a6c66e9c9e604fab1b3031ad849179df1e0bbe3
Fernando Penna <futured@gmail.com>
2026-03-03 16:03:36 -0300

    fix: improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://zamper.dev.br',
   integrations: [sitemap()],
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
